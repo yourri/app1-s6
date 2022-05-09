6ecfc87982483ef77d6b5097c424def081c144be
yourri <lect2803@usherbrooke.ca>
2022-05-09 12:22:28 -0400

    First commit

---
+++ src/lab_ex3.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+int main(int argc, char** argv)
+{
+    std::ifstream file_in;
+
+    if (argc >= 2 && (strcmp(argv[1], "-") != 0)) {
+        file_in.open(argv[1]);
+        if (file_in.is_open()) {
+            std::cin.rdbuf(file_in.rdbuf());
+            std::cerr << "Using " << argv[1] << "..." << std::endl;
+        } else {
+            std::cerr   << "Error: Cannot open '"
+                        << argv[1] 
+                        << "', using stdin (press CTRL-D for EOF)." 
+                        << std::endl;
+        }
+    } else {
+        std::cerr << "Using stdin (press CTRL-D for EOF)." << std::endl;
+    }
+
+    while (!std::cin.eof()) {
+
+        std::string line;
+
+        std::getline(std::cin, line);
+        if (!line.empty()) {
+            int val = atoi(line.c_str());
+            std::cout << val * 2 << std::endl;
+        }
+    }
+
+    if (file_in.is_open()) {
+        file_in.close();
+    }
+
+    return 0;
+}
