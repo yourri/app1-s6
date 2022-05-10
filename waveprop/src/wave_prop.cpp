e10a7fdc9bf3bdb36a81b985bcefd96f09f01f1f
yourri <47328955+yourri@users.noreply.github.com>
2022-05-10 13:30:06 +0000

    Ready

--- waveprop/src/wave_prop.cpp
+++ waveprop/src/wave_prop.cpp
@@ -10,7 +10,7 @@ va4d create_buffer(float value, int n);
 va4d curl_E(va4d E);
 va4d curl_H(va4d H);
 void print_va4d(va4d val_array);
-
+float source(int index);
 
 int main(int argc, char** argv)
 {
@@ -18,19 +18,20 @@ int main(int argc, char** argv)
     int n = 100;
     int source_pos[] = {33,33,50};
     int source_wave = 0.1*sin(0.1*index);
-    va4d E = create_buffer(0, 100);
-    va4d H = create_buffer(0, 100);
-    for(int i = 0; i < 50; i++) {
+    va4d E = create_buffer(0.0, 100);
+    va4d H = create_buffer(0.0, 100);
+    for(int i = 0; i < 700; i++) {
         cout << i << endl;
-        E += (curl_E(E)) * create_buffer(0.1, n);
-        H -= (curl_H(H)) * create_buffer(0.1, n);
+        E += (curl_H(H)) * create_buffer(0.1, n);
+        E[(int)(n/3)][(int)(n/3)][(int)(n/2)][0] +=  source(i);
+        H -= (curl_E(E)) * create_buffer(0.1, n);
     }
-    //print_va4d(curl_E_object);
+    print_va4d(E);
     return 0;
 }
 
 va4d curl_E(va4d val_array){
-    va4d curl_E  = create_buffer(0, 100);
+    va4d curl_E  = create_buffer(0,0, 100);
     // Curl 1 de E
     for(int width=0; width<val_array.size(); width++){
         for(int colonne=0; colonne<val_array[width].size()-1; colonne--){
@@ -162,4 +163,8 @@ va4d create_buffer(float value, int n){
     va3d colonne(line, n);
     va4d width(colonne, n);
     return width;
+}
+
+float source(int index) {
+    return 0.1*sin(0.1 * index);
 }
