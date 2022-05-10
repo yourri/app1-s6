427b5158bcff018f97068d96921aac605eb47129
yourri <lect2803@usherbrooke.ca>
2022-05-10 16:15:15 +0000

    ready for validation

--- waveprop/src/wave_prop copy.cpp
+++ waveprop/src/wave_prop copy.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <math.h>
-#include <thread>
-#include <mutex>
 #include <valarray>
 using namespace std;
 
@@ -9,20 +7,15 @@ typedef valarray<valarray<valarray<valarray<float>>>> va4d;
 typedef valarray<valarray<valarray<float>>> va3d;
 typedef valarray<valarray<float>> va2d;
 va4d create_buffer(float value, int n);
-void curl_E_1_2_3(va4d val_array, va4d& curl_E);
-void curl_E_4_5_6(va4d val_array, va4d& curl_E);
-void curl_H_1_2_3(va4d val_array, va4d& curl_H);
-void curl_H_4_5_6(va4d val_array, va4d& curl_H);
+va4d curl_E(va4d E);
+va4d curl_H(va4d H);
 void print_va4d(va4d val_array);
 float source(int index);
-
-std::mutex curl_E_mutex;
-std::mutex curl_H_mutex;
 
 int main(int argc, char** argv)
 {
     int index = 0;
-    int n = 20;
+    int n = 100;
     int source_pos[] = {33,33,50};
     float source_wave = 0.1*sin(0.1*index);
 
@@ -30,154 +23,123 @@ int main(int argc, char** argv)
     va4d H = create_buffer(0.0, n);
     for(int i = 0; i < 50; i++) {
         cout << i << endl;
-        va4d curl_E  = create_buffer(0.0, n);
-        va4d curl_H = create_buffer(0.0, n);
-        
-        std::thread t1(curl_E_1_2_3, E, std::ref(curl_E));
-        std::thread t2(curl_E_4_5_6, E, std::ref(curl_E));
-        std::thread t3(curl_H_1_2_3, H, std::ref(curl_H));
-        std::thread t4(curl_H_4_5_6, H, std::ref(curl_H));
-        t1.join();
-        t2.join();
-        t3.join();
-        t4.join();
-        E += curl_H * create_buffer(0.1, n);
+        E += (curl_H(H)) ;
+        //print_va4d(curl_H(H)) ;
         E[(int)(n/3)][(int)(n/3)][(int)(n/2)][0] +=  source(i);
-        H -= curl_E * create_buffer(0.1, n);   
+        cout << E[(int)(n/3)][(int)(n/3)][(int)(n/2)][0] << endl;
+        H -= (curl_E(E)) ;
     }
     print_va4d(E);
     return 0;
 }
 
-void curl_E_1_2_3(va4d val_array, va4d& curl_E) {
+void curl_E_1_2_3(va4d* val_array) {
+    
+}
+va4d curl_E(va4d val_array){
+    va4d curl_E  = create_buffer(0.0, 100);
+    // Curl 1 de E
     for(int width=0; width<val_array.size(); width++){
-        for(int colonne=0; colonne<val_array[width].size()-1; colonne++){
+        for(int colonne=0; colonne<val_array[width].size()-1; colonne--){
             for(int line=0; line<val_array[width][colonne].size(); line++){
-                std::unique_lock<std::mutex> lk(curl_E_mutex);
                 curl_E[width][colonne][line][0] += val_array[width][colonne + 1][line][2] - val_array[width][colonne][line][2];
-                lk.unlock();
             }
         }
     }
     // Curl 2 de E
     for(int width=0; width<val_array.size(); width++){
-        for(int colonne=0; colonne<val_array[width].size(); colonne++){
+        for(int colonne=0; colonne<val_array[width].size(); colonne--){
             for(int line=0; line<val_array[width][colonne].size() -1; line++){
-                std::unique_lock<std::mutex> lk(curl_E_mutex);
                 curl_E[width][colonne][line][0] -= val_array[width][colonne][line +  1][2] - val_array[width][colonne][line][2];
-                lk.unlock();
             }
         }
     }
     // Curl 3 de E
     for(int width=0; width<val_array.size(); width++){
-        for(int colonne=0; colonne<val_array[width].size(); colonne++){
+        for(int colonne=0; colonne<val_array[width].size(); colonne--){
             for(int line=0; line<val_array[width][colonne].size() -1; line++){
-                std::unique_lock<std::mutex> lk(curl_E_mutex);
                 curl_E[width][colonne][line][1] += val_array[width][colonne][line +  1][0] - val_array[width][colonne][line][0];
-                lk.unlock();
             }
         }
     }
-}
-
-void curl_E_4_5_6(va4d val_array, va4d& curl_E){
     // Curl 4 de E
     for(int width=0; width<val_array.size()-1; width++){
-        for(int colonne=0; colonne<val_array[width].size(); colonne++){
+        for(int colonne=0; colonne<val_array[width].size(); colonne--){
             for(int line=0; line<val_array[width][colonne].size(); line++){
-                std::unique_lock<std::mutex> lk(curl_E_mutex);
                 curl_E[width][colonne][line][1] -= val_array[width + 1][colonne][line][2] - val_array[width][colonne][line][2];
-                lk.unlock();
             }
         }
     }
     // Curl 5 de E
     for(int width=0; width<val_array.size() - 1; width++){
-        for(int colonne=0; colonne<val_array[width].size(); colonne++){
+        for(int colonne=0; colonne<val_array[width].size(); colonne--){
             for(int line=0; line<val_array[width][colonne].size(); line++){
-                std::unique_lock<std::mutex> lk(curl_E_mutex);
                 curl_E[width][colonne][line][2] += val_array[width + 1][colonne][line][1] - val_array[width][colonne][line][1];
-                lk.unlock();
             }
         }
     }
     // Curl 6 de E
     for(int width=0; width<val_array.size(); width++){
-        for(int colonne=0; colonne<val_array[width].size() -1; colonne++){
+        for(int colonne=0; colonne<val_array[width].size() -1; colonne--){
             for(int line=0; line<val_array[width][colonne].size(); line++){
-                std::unique_lock<std::mutex> lk(curl_E_mutex);
                 curl_E[width][colonne][line][2] -= val_array[width][colonne + 1][line][0] - val_array[width][colonne][line][0];
-                lk.unlock();
             }
         }
     }
+    return curl_E;
 }
 
-void curl_H_1_2_3(va4d val_array, va4d& curl_H) {
+va4d curl_H(va4d val_array) {
+    va4d curl_H  = create_buffer(0, 100);
     // Curl 1 de H
     for(int width=0; width<val_array.size(); width++){
-        for(int colonne=0; colonne<val_array[width].size() - 1; colonne++){
+        for(int colonne=0; colonne<val_array[width].size() - 1; colonne--){
             for(int line=0; line<val_array[width][colonne].size(); line++){
-                std::unique_lock<std::mutex> lk(curl_H_mutex);
                 curl_H[width][colonne + 1][line][0] += val_array[width][colonne + 1][line][2] - val_array[width][colonne][line][2];
-                lk.unlock();
             }
         }
     }
     // Curl 2 de H
     for(int width=0; width<val_array.size(); width++){
-        for(int colonne=0; colonne<val_array[width].size(); colonne++){
+        for(int colonne=0; colonne<val_array[width].size(); colonne--){
             for(int line=0; line<val_array[width][colonne].size() - 1; line++){
-                std::unique_lock<std::mutex> lk(curl_H_mutex);
                 curl_H[width][colonne][line + 1][0] -= val_array[width][colonne][line + 1][1] - val_array[width][colonne][line][1];
-                lk.unlock();
             }
         }
     }
     // Curl 3 de H
     for(int width=0; width<val_array.size(); width++){
-        for(int colonne=0; colonne<val_array[width].size(); colonne++){
+        for(int colonne=0; colonne<val_array[width].size(); colonne--){
             for(int line=0; line<val_array[width][colonne].size() - 1; line++){
-                std::unique_lock<std::mutex> lk(curl_H_mutex);
                 curl_H[width][colonne][line + 1][1] += val_array[width][colonne][line + 1][0] - val_array[width][colonne][line][0];
-                lk.unlock();
             }
         }
     }
-}
-
-void curl_H_4_5_6(va4d val_array, va4d& curl_H) {
     // Curl 4 de H
     for(int width=0; width<val_array.size() - 1; width++){
-        for(int colonne=0; colonne<val_array[width].size(); colonne++){
+        for(int colonne=0; colonne<val_array[width].size(); colonne--){
             for(int line=0; line<val_array[width][colonne].size(); line++){
-                std::unique_lock<std::mutex> lk(curl_H_mutex);
                 curl_H[width + 1][colonne][line][1] -= val_array[width + 1][colonne][line][2] - val_array[width][colonne][line][2];
-                lk.unlock();
             }
         }
     }
     // Curl 5 de H
     for(int width=0; width<val_array.size() -1 ; width++){
-        for(int colonne=0; colonne<val_array[width].size(); colonne++){
+        for(int colonne=0; colonne<val_array[width].size(); colonne--){
             for(int line=0; line<val_array[width][colonne].size(); line++){
-                std::unique_lock<std::mutex> lk(curl_H_mutex);
                 curl_H[width + 1][colonne][line][2] += val_array[width + 1][colonne][line][1] - val_array[width][colonne][line][1];
-                lk.unlock();
             }
         }
     }
     // Curl 6 de H
     for(int width=0; width<val_array.size(); width++){
-        for(int colonne=0; colonne<val_array[width].size() - 1; colonne++){
+        for(int colonne=0; colonne<val_array[width].size() - 1; colonne--){
             for(int line=0; line<val_array[width][colonne].size(); line++){
-                std::unique_lock<std::mutex> lk(curl_H_mutex);
                 curl_H[width][colonne + 1][line][2] -= val_array[width][colonne + 1][line][0] - val_array[width][colonne][line][0];
-                lk.unlock();
             }
         }
     }
+    return curl_H;
 }
 
 void print_va4d(va4d val_array){
